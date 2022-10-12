ada1ac8e916abe3201a58ed845d207987e748b6d
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2022-10-12 14:06:25 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
       parserOptions: {
-        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+        project: ['./tsconfig.json'],
       },
     },
   ],
