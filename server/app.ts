60b956dd654342e90936f03de8ec3122388f6042
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-13 12:00:28 +0300

    setup the app and download morgan relates #53

--- server/app.ts
+++ server/app.ts
@@ -2,14 +2,29 @@ import express from 'express';
 import compression from 'compression';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-// import router from './routes';
+import { join } from 'path';
+import morgan from 'morgan';
+
+// const morgan = require('morgan');
 
 const app = express();
+
 const {
   env: { PORT, NODE_ENV },
 } = process;
 
 dotenv.config();
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+if (NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+// app.use('/api/v1', router);
 app.set('port', PORT || 5000);
 
 app.use(express.json());
