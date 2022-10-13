590f71d2a46304e014aa51765a481ab6926cc887
Mai elhajeen <maielhajeen2001@gmail.com>
2022-10-13 15:53:36 +0300

    adding fake data

--- server/database/config/fakeData.ts
+++ server/database/config/fakeData.ts
@@ -46,4 +46,58 @@ const Advertisements = [
 
 ];
 
-export { Announcements, Advertisements };
+const complaints = [
+  {
+    title: 'فاتورة رقم 1',
+    description: 'تفاصيل الفاتورة',
+    creationDate: '00/00/0000',
+    isSolved: true,
+    userId: 1,
+  },
+  {
+    title: 'فاتورة رقم 2',
+    description: 'تفاصيل الفاتورة',
+    creationDate: '00/00/0000',
+    isSolved: true,
+    userId: 2,
+  },
+  {
+    title: 'فاتورة رقم 3',
+    description: 'تفاصيل الفاتورة',
+    creationDate: '00/00/0000',
+    isSolved: true,
+    userId: 3,
+  },
+];
+
+const contactUs = [
+  {
+    name: 'محمد أمين',
+    email: 'تفاصيل الفاتورة',
+    phone: '000-000-000',
+    subject: 'حارس عمارة',
+    description: 'تفاصيل الرسالة',
+    creation_date: '00/00/0000',
+  },
+  {
+    name: 'محمد أمين',
+    email: 'تفاصيل الفاتورة',
+    phone: '000-000-000',
+    subject: 'حارس عمارة',
+    description: 'تفاصيل الرسالة',
+    creation_date: '00/00/0000',
+  },
+  {
+    name: 'محمد أمين',
+    email: 'تفاصيل الفاتورة',
+    phone: '000-000-000',
+    subject: 'حارس عمارة',
+    description: 'تفاصيل الرسالة',
+    creation_date: '00/00/0000',
+  },
+];
+
+export {
+  Announcements, Advertisements,
+  complaints, contactUs,
+};
