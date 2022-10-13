739156b2f402288de7db89c0b10a0564d0f99fca
Mai elhajeen <maielhajeen2001@gmail.com>
2022-10-13 12:02:59 +0300

    create fake data for complaints and contact

---
+++ server/database/config/fakeData.ts
@@ -0,0 +1,55 @@
+const complaints = [
+  {
+    title: 'فاتورة رقم 1',
+    description: 'تفاصيل الفاتورة',
+    creationDate: '00/00/0000',
+    isSolved: true,
+    userId: 1,
+  },
+  {
+    title: 'فاتورة رقم 2',
+    description: 'تفاصيل الفاتورة',
+    creationDate: '00/00/0000',
+    isSolved: true,
+    userId: 2,
+  },
+  {
+    title: 'فاتورة رقم 3',
+    description: 'تفاصيل الفاتورة',
+    creationDate: '00/00/0000',
+    isSolved: true,
+    userId: 3,
+  },
+];
+
+const contactUs = [
+  {
+    name: 'محمد أمين',
+    email: 'تفاصيل الفاتورة',
+    phone: '000-000-000',
+    subject: 'حارس عمارة',
+    description: 'تفاصيل الرسالة',
+    creation_date: '00/00/0000',
+  },
+  {
+    name: 'محمد أمين',
+    email: 'تفاصيل الفاتورة',
+    phone: '000-000-000',
+    subject: 'حارس عمارة',
+    description: 'تفاصيل الرسالة',
+    creation_date: '00/00/0000',
+  },
+  {
+    name: 'محمد أمين',
+    email: 'تفاصيل الفاتورة',
+    phone: '000-000-000',
+    subject: 'حارس عمارة',
+    description: 'تفاصيل الرسالة',
+    creation_date: '00/00/0000',
+  },
+];
+
+export {
+  complaints,
+  contactUs,
+};
