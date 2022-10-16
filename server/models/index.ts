1b88c713528f9534ffb6e00fe357f1cd2a12564f
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-16 09:54:10 +0300

    do the relation between flat and bill and user related #43

--- server/models/index.ts
+++ server/models/index.ts
@@ -4,6 +4,16 @@ import BillModule from './Bill';
 import FlatModule from './Flat';
 import ServiceModule from './Service';
 
+BillModule.hasOne(FlatModule, {
+  foreignKey: 'flat_id',
+});
+FlatModule.belongsTo(BillModule);
+
+// FlatModule.hasOne(User, {
+//   foreignKey: 'owner_id',
+// });
+// User.belongsTo(FlatModule);
+
 export {
   BillModule, FlatModule, ServiceModule, Announcement, Advertisement,
 };
