ac8b3fe5bb9d0e7a8b890e640de40ea1238e8d58
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-17 16:26:17 +0300

    Add more tests

--- __tests__/advertisements.test.ts
+++ __tests__/advertisements.test.ts
@@ -12,6 +12,18 @@ describe('advertisement API', () => {
         .expect(200);
       expect(response.body.data.length).toBe(3);
     });
+    test('Advertisement -GET api/v1/advertisements/', async () => {
+        const response = await request(app)
+          .get('/api/v1/advertisements/')
+          .expect(200);
+        expect(response.body.data[0].id).toBe(1);
+      });
+      test('Advertisement -GET api/v1/advertisements/', async () => {
+        const response = await request(app)
+          .get('/api/v1/advertisements/')
+          .expect(200);
+        expect(response.body.data[1].start_date).toBe("20/10/2022");
+      });
   });
 
 
