a721e6e9a1edf443c306724e8ebab0c86db9964d
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-18 15:47:07 +0300

    change all pargraph and header to Title component from ant design related #67

--- client/src/components/landing/flats/FlatCard.tsx
+++ client/src/components/landing/flats/FlatCard.tsx
@@ -1,6 +1,10 @@
-import { Card, Button } from 'antd';
+import {
+  Card, Button, Image, Typography,
+} from 'antd';
 import { PhoneOutlined } from '@ant-design/icons';
 import React from 'react';
+
+const { Title } = Typography;
 
 type FreeFlatType = {
   id: number,
@@ -16,33 +20,33 @@ const FlatCard: React.FC<FlatCardProps> = ({ info }) => (
     className="flatCard"
     hoverable
     style={{ width: 342, height: 444 }}
-    cover={<img alt="example" src="https://images.pexels.com/photos/7534561/pexels-photo-7534561.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />}
+    cover={<Image alt="free_flat" src="https://images.pexels.com/photos/7534561/pexels-photo-7534561.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />}
   >
     <div className="iconsAndArea">
-      <p className="area">{`$ ${info.area}`}</p>
+      <Title level={5} className="area">{`${info.area}م`}</Title>
       <div className="icons">
         <i className="ri-chat-1-fill" />
         <PhoneOutlined />
         <i className="ri-hand-heart-line" />
       </div>
     </div>
-    <p className="firstP">{info.notes}</p>
-    <p className="secondeP">بعض التفاصيل</p>
+    <Title level={3} className="firstP">{info.notes}</Title>
+    <Title level={5} className="secondeP">بعض التفاصيل</Title>
     <div className="details">
       <div className="oneDetail">
-        <p>300</p>
+        <Title level={5}>300</Title>
         <i className="ri-hotel-bed-fill" />
       </div>
       <div className="oneDetail">
-        <p>نوم</p>
+        <Title level={5}>نوم</Title>
         <i className="ri-hotel-bed-fill" />
       </div>
       <div className="oneDetail">
-        <p>نوم</p>
+        <Title level={5}>نوم</Title>
         <i className="ri-hotel-bed-fill" />
       </div>
       <div className="oneDetail">
-        <p>نوم</p>
+        <Title level={5}>نوم</Title>
         <i className="ri-hotel-bed-fill" />
       </div>
     </div>
--- client/src/components/landing/flats/FlatContainer.tsx
+++ client/src/components/landing/flats/FlatContainer.tsx
@@ -1,8 +1,10 @@
-import { message } from 'antd';
+import { message, Typography } from 'antd';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Loading, NoData } from '../index';
 import FlatCard from './FlatCard';
+
+const { Title } = Typography;
 
 type FreeFlatType = {
   id: number,
@@ -29,8 +31,8 @@ const FlatContainer: React.FC = () => {
   return (
 
     <div className="flatContainerComponent">
-      <h2>شقق متاحة</h2>
-      <p>هنالك شقق متوفرة بمساحات مختلفة</p>
+      <Title level={2} className="HeaderOfFLat">شقق متاحة</Title>
+      <Title level={5} className="PargraphOfFLat">هنالك شقق متوفرة بمساحات مختلفة</Title>
       <div className="flatContainer">
         {
           // eslint-disable-next-line no-nested-ternary
