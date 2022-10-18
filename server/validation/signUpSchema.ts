6bd7ef89044752ef3b1c8103580c4d164f930206
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-18 16:51:04 +0300

    edit signupScheme relates #79

--- server/validation/signUpSchema.ts
+++ server/validation/signUpSchema.ts
@@ -1,17 +1,17 @@
 import * as yup from 'yup';
 
 const SignUpSchema = yup.object({
-  firstName: yup.string().required('الاسم مطلوب'),
-  lastName: yup.string().required('الاسم مطلوب'),
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
   phoneNumber: yup.string()
-    .required('حقل رقم الهاتف مطلوب')
-    .min(7, 'رقم الهاتف يجب أن تكون أكثر من {0}')
-    .max(14, 'رقم الهاتف يجب أن تكون أقل من {0}'),
-  email: yup.string().email('يجب أن يكون بريدك الإلكتروني بريدًا إلكترونيًا صالحًا').required('مطلوب بريدك الإلكتروني'),
+    .required()
+    .min(7)
+    .max(14),
+  email: yup.string().email().required(),
   password: yup.string()
-    .required('جقل كلمة المرور مطلوبرقم الهاتف مطلوب')
-    .min(8, 'كلمة المرور يجب أن تكون أكثر من {0}')
-    .max(16, 'كلمة المرور يجب أن تكون أقل من {0}'),
+    .required()
+    .min(8)
+    .max(16),
 
 });
 
