505d01171600f32ebe700619082e6b05e5db3ef3
mohjaps <moh.ja.ps@gmail.com>
2022-10-18 15:10:37 +0300

    login controller

---
+++ server/controllers/Auth/login.ts
@@ -0,0 +1,39 @@
+import bcrypt from 'bcrypt';
+import loginValidation from '../../validation';
+import { UserModel } from '../../models';
+import CustomError from '../../helpers';
+import { GenerateToken } from '../../middleware';
+
+const login = async (req, res, next) => {
+  try {
+    const { phoneNumber, password } = req.body;
+    await loginValidation.validate({ phoneNumber, password });
+    const user = await UserModel.findOne({ where: { phone_number: phoneNumber } });
+
+    if (!user) {
+      throw new CustomError(
+        404,
+        'المستخدم غير موجود',
+      );
+    }
+    const {
+      role, id, hashed_password,
+    } = user;
+
+    const comparePasswordResult = await bcrypt.compare(password, hashed_password);
+    if (!comparePasswordResult) {
+      throw new CustomError(
+        404,
+        'كلمة المرور خاطئة',
+      );
+    }
+
+    GenerateToken({
+      phoneNumber, id, role,
+    }, res, next);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default login;
