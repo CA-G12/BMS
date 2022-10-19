a2efb75fb9d87bfeb3a39e91869843e521510cb8
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-19 13:08:16 +0300

    add a tests for a post services related #26

---
+++ __tests__/services.ts
@@ -0,0 +1,73 @@
+import request from 'supertest';
+import app from '../server/app';
+import insertDB from '../server/database/config/build';
+import sequelize from '../server/database/config/connection';
+
+beforeAll(insertDB);
+
+describe('Services API', () => {
+    test('Services -post api/v1/services/', async () => {
+      const response = await request(app)
+        .post('/api/v1/services')
+        .send({
+            id:1,
+            name: 'خدمات عامة',
+            price: 100,
+            is_fixed: true,
+            description: 'الخدمات العامة تشمل النظافة والمصعد والعناية بالحديقة والامان',
+            "is_open": true
+
+        })
+        .expect(200);
+      expect(response.body.data.price).toBe("100");
+    });
+    test('Services -post api/v1/services/', async () => {
+        const response = await request(app)
+          .post('/api/v1/services')
+          .send({
+              id:1,
+              name: 'خدمات عامة',
+              price: 900,
+              is_fixed: true,
+              description: 'الخدمات العامة تشمل النظافة والمصعد والعناية بالحديقة والامان',
+              "is_open": true
+
+          })
+          .expect(200);
+        expect(response.body.data.price).toBe("900");
+      });
+      test('Services -post api/v1/services/', async () => {
+        const response = await request(app)
+          .post('/api/v1/services')
+          .send({
+              id:1,
+              name: 'خدمات',
+              price: 100,
+              is_fixed: true,
+              description: 'الخدمات العامة تشمل النظافة والمصعد والعناية بالحديقة والامان',
+              "is_open": true
+
+          })
+          .expect(200);
+        expect(response.body.data.name).toBe("خدمات");
+      });
+      test('Services -post api/v1/services/', async () => {
+        const response = await request(app)
+          .post('/api/v1/services')
+          .send({
+              id:1,
+              name: 'خدمات عامة',
+              price: 120,
+              is_fixed: true,
+              description: 'الخدمات العامة تشمل النظافة والمصعد والعناية بالحديقة والامان',
+              "is_open": true
+
+          })
+          .expect(200);
+        expect(response.body.data.price).toBe("120");
+      });
+
+  });
+
+
+afterAll(async () => sequelize.close());
