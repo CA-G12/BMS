46923114d1c01f12a1fb7001a32bcc963f140e0a
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-19 12:16:27 +0300

    add tests for a get services related #30

--- __tests__/flat.ts
+++ __tests__/flat.ts
@@ -10,7 +10,7 @@ describe('Flat API', () => {
       const response = await request(app)
         .get('/api/v1/flats/')
         .expect(200);
-      expect(response.body.data.length).toBe(4);
+      expect(response.body.data.length).toBe(3);
     });
     test('Flat -GET api/v1/flats/', async () => {
         const response = await request(app)
@@ -36,7 +36,6 @@ describe('Flat API', () => {
           .expect(200);
         expect(response.body.data[0].UserId).toBe(1);
       });
-
   });
 
 
---
+++ __tests__/services.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import app from '../server/app';
+import insertDB from '../server/database/config/build';
+import sequelize from '../server/database/config/connection';
+
+beforeAll(insertDB);
+
+describe('Services API', () => {
+    test('Flat -GET api/v1/services/', async () => {
+        const response = await request(app)
+          .get('/api/v1/services/')
+          .expect(200);
+        expect(response.body.data[0].price).toBe("100");
+      });
+      test('Flat -GET api/v1/services/', async () => {
+        const response = await request(app)
+          .get('/api/v1/services/')
+          .expect(200);
+        expect(response.body.data[1].price).toBe("0");
+      });
+      test('Flat -GET api/v1/services/', async () => {
+        const response = await request(app)
+          .get('/api/v1/services/')
+          .expect(200);
+        expect(response.body.data[2].price).toBe("0");
+      });
+      test('Flat -GET api/v1/services/', async () => {
+        const response = await request(app)
+          .get('/api/v1/services/')
+          .expect(200);
+        expect(response.body.data[0].is_fixed).toBe(true);
+      });
+  });
+
+
+afterAll(async () => sequelize.close());
