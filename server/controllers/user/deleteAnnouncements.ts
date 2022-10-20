b5cf30fc422aeacd81ff5815e64dcd95dc9c1fe8
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-20 10:38:28 +0300

    handle the id if its not a number or not a valid number or vaild number but dose not exist related #14

--- server/controllers/user/deleteAnnouncements.ts
+++ server/controllers/user/deleteAnnouncements.ts
@@ -1,13 +1,23 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable max-len */
 import { Request, Response, NextFunction } from 'express';
 import { AnnouncementModel } from '../../models';
 
 export default async (req:Request, res:Response, next:NextFunction) => {
   try {
     const { announcementsId } = req.params;
-    await AnnouncementModel.destroy({ where: { announcementsId } });
-    res.send('Announcement Deleted Successfully');
+    const convertId:number = +(announcementsId);
+    if (Number.isNaN(convertId)) {
+      res.send('Announcement Id must be a number');
+    } else if (convertId < 1) {
+      res.send('Announcement Id must be greater than 0');
+    }
+
+    const data = await AnnouncementModel.destroy({ where: { id: announcementsId } });
+    if (!data) {
+      res.send('There is no item have this id');
+    } else {
+      res.send('Announcement Deleted Successfully');
+    }
   } catch (err) {
     next(err);
   }
