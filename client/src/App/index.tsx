f2d69074edf719e3779caaacb59326ddb2c0448a
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-20 19:53:17 +0300

    handle routes in router file rlates #93

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -2,24 +2,10 @@ import { FC } from 'react';
 import './style.css';
 import 'remixicon/fonts/remixicon.css';
 import 'antd/dist/antd.css';
-import { Routes, Route } from 'react-router-dom';
-import { AdminDashboard } from '../Layouts';
-import { LandingPage } from '../pages';
-import {
-  Services, Bills, Flats,
-} from '../components/adminDashboard';
+import { Outlet } from 'react-router-dom';
 
 const App: FC = () => (
-  <Routes>
-    <Route path="/" element={<LandingPage />} />
-    <Route path="/admin" element={<AdminDashboard />}>
-      <Route index element={<h1>statistics</h1>} />
-      <Route path="services" element={<Services />} />
-      <Route path="flats" element={<Flats />} />
-      <Route path="bills" element={<Bills />} />
-    </Route>
-    <Route path="*" element={<h1>page not found</h1>} />
-  </Routes>
+  <div><Outlet /></div>
 );
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,16 @@
 /* eslint-disable import/extensions */
 import ReactDOM from 'react-dom/client';
 import { ConfigProvider } from 'antd';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import ar_EG from 'antd/es/locale/ar_EG';
 import App from './App';
+import router from './router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <ConfigProvider direction="rtl" locale={ar_EG}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </ConfigProvider>,
 );
