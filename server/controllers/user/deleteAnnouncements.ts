4754abcef722aa6d6d1c33dd68f6fb6f8fcad22e
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-20 11:55:50 +0300

    send the message by json msg related #14

--- server/controllers/user/deleteAnnouncements.ts
+++ server/controllers/user/deleteAnnouncements.ts
@@ -7,16 +7,16 @@ export default async (req:Request, res:Response, next:NextFunction) => {
     const { announcementsId } = req.params;
     const convertId:number = +(announcementsId);
     if (Number.isNaN(convertId)) {
-      res.send('Announcement Id must be a number');
+      res.json({ msg: 'Announcement Id must be a number' });
     } else if (convertId < 1) {
-      res.send('Announcement Id must be greater than 0');
+      res.json({ msg: 'Announcement Id must be greater than 0' });
     }
 
     const data = await AnnouncementModel.destroy({ where: { id: announcementsId } });
     if (!data) {
-      res.send('There is no item have this id');
+      res.json({ msg: 'There is no item have this id' });
     } else {
-      res.send('Announcement Deleted Successfully');
+      res.json({ msg: 'Announcement Deleted Successfully' });
     }
   } catch (err) {
     next(err);
