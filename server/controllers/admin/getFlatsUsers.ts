40ac9ea4bb71cfe3df41835c90b6068e43e0e132
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-23 10:25:38 +0200

    make pagination for get flatsUsers relates #88

--- server/controllers/admin/getFlatsUsers.ts
+++ server/controllers/admin/getFlatsUsers.ts
@@ -1,14 +1,23 @@
 import {
   Request, Response, NextFunction,
 } from 'express';
+import CustomError from '../../helpers';
 import { FlatModel, UserModel } from '../../models';
 
 export default async (req:Request, res:Response, next:NextFunction) => {
   try {
+    const limitNum = 2;
+    const { pageNum } = req.query;
+    if ((+pageNum) <= 0) {
+      throw new CustomError(400, 'bad request');
+    }
+    const offsetNum = (+(pageNum) - 1) * limitNum;
     const data = await FlatModel.findAll({
       include: [{
         model: UserModel,
       }],
+      offset: offsetNum,
+      limit: limitNum,
     });
     res.json({ data });
   } catch (err) {
