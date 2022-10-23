7b3a178698be9eab53f7fbf20abcc9be101bc0a6
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2022-10-23 16:26:41 +0300

    Change file structure

--- server/controllers/announcement/postAnnouncement.ts
+++ server/controllers/announcement/postAnnouncement.ts
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable max-len */
 import { Request, Response, NextFunction } from 'express';
-import { AnnouncementModel } from '../../../models';
+import { AnnouncementModel } from '../../models';
 
 export default async (req:Request, res:Response, next:NextFunction) => {
   try {
-    const { id } = req.params;
     const { title, start_date, end_date } = await req.body;
-    const data = await AnnouncementModel.update({ title, start_date, end_date }, { where: { id }, returning: true });
-    res.json({ data: data[1] });
+    const data = await AnnouncementModel.create({ title, start_date, end_date });
+    res.json({ data });
   } catch (err) {
     next(err);
   }
--- server/controllers/user/index.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as getFlats } from './getFlats';
-export { default as getAdvertisements } from './getAdvertisemsnts';
-export {
-  getAnnouncement, postAnnouncement, getAnnouncementById, putAnnouncement, deleteAnnouncement,
-} from './announcement';
