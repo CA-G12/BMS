da8240014a76658c2d918f4b63d7c9e84cc16bc9
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-23 10:27:40 +0200

    modify tests relates #88

--- __tests__/flatsUsers.ts
+++ __tests__/flatsUsers.ts
@@ -8,43 +8,43 @@ beforeAll(insertDB);
 describe('Flat API', () => {
     test('FlatsUsers -GET api/v1/flatsUsers', async () => {
       const response = await request(app)
-        .get('/api/v1/flatsUsers')
+        .get('/api/v1/flatsUsers?pageNum=2')
         .expect(200);
-      expect(response.body.data.length).toBe(4);
+      expect(response.body.data.length).toBe(2);
     });
     test('FlatsUsers -GET api/v1/flatsUsers', async () => {
         const response = await request(app)
-          .get('/api/v1/flatsUsers')
+          .get('/api/v1/flatsUsers?pageNum=1')
           .expect(200);
         expect(response.body.data[0].flat_number).toBe(101);
       });
       test('FlatsUsers -GET api/v1/flatsUsers', async () => {
         const response = await request(app)
-          .get('/api/v1/flatsUsers')
+          .get('/api/v1/flatsUsers?pageNum=1')
           .expect(200);
         expect(response.body.data[1].area).toBe("160");
       });
       test('FlatsUsers -GET api/v1/flatsUsers', async () => {
         const response = await request(app)
-          .get('/api/v1/flatsUsers')
+          .get('/api/v1/flatsUsers?pageNum=1')
           .expect(200);
-        expect(response.body.data[2].UserId).toBe(3);
+        expect(response.body.data[1].UserId).toBe(2);
       });
       test('FlatsUsers -GET api/v1/flatsUsers', async () => {
         const response = await request(app)
-          .get('/api/v1/flatsUsers')
+          .get('/api/v1/flatsUsers?pageNum=1')
           .expect(200);
         expect(response.body.data[0].UserId).toBe(1);
       });
       test('FlatsUsers -GET api/v1/flatsUsers', async () => {
         const response = await request(app)
-          .get('/api/v1/flatsUsers')
+          .get('/api/v1/flatsUsers?pageNum=1')
           .expect(200);
         expect(response.body.data[0].User.id).toBe(1);
       });
       test('FlatsUsers -GET api/v1/flatsUsers', async () => {
         const response = await request(app)
-          .get('/api/v1/flatsUsers')
+          .get('/api/v1/flatsUsers?pageNum=1')
           .expect(200);
         expect(response.body.data[0].User.email).toBe("jcotte0@people.com.cn");
       });
