b646ab747d3309b3f3e96dc2c0551e94fcf277e9
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2022-10-24 11:53:20 +0300

    Remove unnecessary commented line for eslint

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-/* eslint-disable import/prefer-default-export */
 export { addContact } from './contactus';
 export {
   getAnnouncement, postAnnouncement, getAnnouncementById, putAnnouncement, deleteAnnouncement,
