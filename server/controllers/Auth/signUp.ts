8c29fb09104da77fedb07c9f366664785417140a
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-24 10:11:51 +0200

    make eslint working

--- server/controllers/Auth/signUp.ts
+++ server/controllers/Auth/signUp.ts
@@ -8,28 +8,33 @@ const signUp = async (req:Request, res:Response, next:NextFunction) => {
   try {
     const {
       firstName, lastName, phoneNumber, email, password,
-    } = 
-    await signUpSchema.validate(
+    } = await signUpSchema.validate(
       req.body,
-    { abortEarly: false });
+      { abortEarly: false },
+    );
     const hashedPassword = await hash(password, 10);
-    const [user, created] = await UserModel.findOrCreate({
-        where: { phone_number: phoneNumber } ,
-        defaults: {
-            first_name: firstName, last_name: lastName, email, phone_number: phoneNumber, hashed_password: hashedPassword, role: 'user'
-          }
-      });
+    const [created] = await UserModel.findOrCreate({
+      where: { phone_number: phoneNumber },
+      defaults: {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        phone_number: phoneNumber,
+        hashed_password: hashedPassword,
+        role: 'user',
+      },
+    });
 
-      if(!created){
-            throw new CustomError(
+    if (!created) {
+      throw new CustomError(
         400,
         'Phone Number Already Exist',
       );
-      }
-      res.json({ message: 'Logged in Successfully' });
+    }
+    res.json({ message: 'User Created Successfully' });
   } catch (err) {
     if (err.name === 'ValidationError') {
-        return next(new CustomError(400, err.errors));
+      return next(new CustomError(400, err.errors));
     }
     next(err);
   }
