f159007edc4302ac241a4e176e37c2223e54f909
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-24 14:00:30 +0300

    addd tests for put services related #26

--- __tests__/services.ts
+++ __tests__/services.ts
@@ -91,6 +91,59 @@ describe('Services API', () => {
           .expect(200);
         expect(response.body.data[0].is_fixed).toBe(true);
       });
+
+      test('Services -put api/v1/services/', async () => {
+        const response = await request(app)
+          .put('/api/v1/services')
+          .send({
+              id:1,
+              name: 'خدمات عامة',
+              price: 100,
+              is_fixed: true,
+              description: 'الخدمات العامة تشمل النظافة والمصعد والعناية بالحديقة والامان',
+          })
+          .expect(200);
+        expect(response.body.data[0].price).toBe(100);
+      });
+      test('Services -put api/v1/services/', async () => {
+          const response = await request(app)
+            .put('/api/v1/services')
+            .send({
+                id:1,
+                name: 'خدمات عامة',
+                price: 900,
+                is_fixed: true,
+                description: 'الخدمات العامة تشمل النظافة والمصعد والعناية بالحديقة والامان',
+            })
+            .expect(200);
+          expect(response.body.data[0].price).toBe(900);
+        });
+        test('Services -put api/v1/services/', async () => {
+          const response = await request(app)
+            .put('/api/v1/services')
+            .send({
+                id:1,
+                name: 'خدمات',
+                price: 100,
+                is_fixed: true,
+                description: 'الخدمات العامة تشمل النظافة والمصعد والعناية بالحديقة والامان',
+            })
+            .expect(200);
+          expect(response.body.data[0].name).toBe("خدمات");
+        });
+        test('Services -put api/v1/services/', async () => {
+          const response = await request(app)
+            .put('/api/v1/services')
+            .send({
+                id:1,
+                name: 'خدمات عامة',
+                price: 120,
+                is_fixed: true,
+                description: 'الخدمات العامة تشمل النظافة والمصعد والعناية بالحديقة والامان',
+            })
+            .expect(200);
+          expect(response.body.data[0].price).toBe(120);
+        });
   });
 
 
