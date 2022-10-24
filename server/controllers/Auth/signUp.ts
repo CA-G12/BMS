77bca73cd2c45a1a4a623eb8b1e3eaf86a16dcd7
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-24 09:35:26 +0200

    edit the sign up controller

--- server/controllers/Auth/signUp.ts
+++ server/controllers/Auth/signUp.ts
@@ -9,30 +9,30 @@ const signUp = async (req:Request, res:Response, next:NextFunction) => {
   try {
     const {
       firstName, lastName, phoneNumber, email, password,
-    } = req.body;
-    await signUpSchema.validateSync({
-      firstName, lastName, phoneNumber, email, password,
-    }, { abortEarly: false });
-    // const users = await UserModel.findAll({ limit: 3 });
-    const user = await UserModel.count({ where: { phone_number: phoneNumber } });
-    if (user) {
-      throw new CustomError(
+    } = 
+    await signUpSchema.validateSync(
+      req.body,
+    { abortEarly: false });
+    const hashedPassword = await hash(password, 10);
+    const [user, created] = await UserModel.findOrCreate({
+        where: { phone_number: phoneNumber } ,
+        defaults: {
+            first_name: firstName, last_name: lastName, email, phone_number: phoneNumber, hashed_password: hashedPassword, role: 'user'
+          }
+      });
+
+      if(!created){
+            throw new CustomError(
         400,
         'Phone Number Already Exist',
       );
-    }
-    const hashedPassword = await hash(password, 10);
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { id, role } = await UserModel.create({
-      // eslint-disable-next-line max-len
-      first_name: firstName, last_name: lastName, email, phone_number: phoneNumber, hashed_password: hashedPassword, role: 'user',
-    });
-    GenerateToken({
-      id, role,
-    }, res, next);
+      }
+      const role = user.role;
+      const id = user.id 
+      GenerateToken({
+        id, role,
+}, res, next);
   } catch (err) {
-    console.log('err: ', err);
     if (err.name === 'ValidationError') {
       return next(new CustomError(400, err.errors));
     }
