51f9b1db8b52f9126e7be6ab6bd4ac760e23913b
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-24 22:48:01 +0200

    handle signup in front side relates #103

--- client/src/components/adminDashboard/flats.tsx
+++ client/src/components/adminDashboard/flats.tsx
@@ -1,2 +1,13 @@
-const Flats = () => <h1>flats</h1>;
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
+
+const Flats = () => (
+  <>
+    <h1>flats</h1>
+    <Button type="primary" htmlType="submit">
+      <Link to="/admin/auth/signup">إضافة مستخدم</Link>
+
+    </Button>
+  </>
+);
 export default Flats;
