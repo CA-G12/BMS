224cce7abadc6a0235c0356a3311f9ed597bdfec
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-24 13:35:20 +0300

    add the validation for post services related #26

--- server/controllers/services/postServices.ts
+++ server/controllers/services/postServices.ts
@@ -1,17 +1,22 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../../helpers';
 import { ServiceModel } from '../../models';
+import { servicesSchema } from '../../validation';
 
 export default async (req:Request, res:Response, next: NextFunction) => {
   try {
     const {
       name, price, is_fixed, description, is_open,
-    } = await req.body;
+    } = await servicesSchema.validate(req.body, { abortEarly: false });
     const data = await ServiceModel.create({
       name, price, is_fixed, description, is_open,
     });
-    res.json({ data });
+    res.status(201).json({ data });
   } catch (err) {
-    next(err);
+    if (err.name === 'ValidationError') {
+      return next(new CustomError(400, err.errors));
+    }
+    return next(err);
   }
 };
