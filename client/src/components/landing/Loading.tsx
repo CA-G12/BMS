0d119626909aef9ef10217d8662cce526ca0119d
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-25 12:58:04 +0300

    change the style of loading realted #98

--- client/src/components/landing/Loading.tsx
+++ client/src/components/landing/Loading.tsx
@@ -2,7 +2,12 @@ import { Space, Spin } from 'antd';
 import React from 'react';
 
 const Loading: React.FC = () => (
-  <Space size="middle" style={{ margin: '0px auto' }}>
+  <Space
+    size="middle"
+    style={{
+      margin: '0px auto', display: 'flex', justifyContent: 'center', height: '100%',
+    }}
+  >
     <Spin size="large" />
   </Space>
 );
