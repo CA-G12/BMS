c980f8d1b13fa471c9a55df782e597edcc4a750e
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-25 21:01:23 +0300

    create a add service realted #98

--- client/src/components/adminDashboard/services/addService.tsx
+++ client/src/components/adminDashboard/services/addService.tsx
@@ -1,19 +1,36 @@
 import {
-  Typography, Button, Form, Input,
+  Typography, Form, Input, Switch, Button,
 } from 'antd';
 import React, { useState } from 'react';
 import '../../../App/style.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { InferAddServicesModel } from '../../../Models/addService';
 
 const { Title } = Typography;
 
 type RequiredMark = boolean | 'optional';
 
 const AddService: React.FC = () => {
+  const navigate = useNavigate();
+
+  // const onChange = (checked: boolean) => {
+  //   console.log(`switch to ${checked}`);
+  // };
   const [form] = Form.useForm();
   const [requiredMark, setRequiredMarkType] = useState<RequiredMark>('optional');
 
   const onRequiredTypeChange = ({ requiredMarkValue }: { requiredMarkValue: RequiredMark }) => {
     setRequiredMarkType(requiredMarkValue);
+  };
+
+  const onFinish = (values : InferAddServicesModel) => {
+    console.log('values: ', { ...values });
+    // eslint-disable-next-line no-void
+    axios.post('/api/v1/services', values)
+      .then(() => navigate('/admin/services'))
+      // .then(async () => { await message.success('تم إنشاء خدمة جديدة بنجاح'); })
+      .catch(() => console.log('error'));
   };
 
   return (
@@ -26,18 +43,26 @@ const AddService: React.FC = () => {
         initialValues={{ requiredMarkValue: requiredMark }}
         onValuesChange={onRequiredTypeChange}
         requiredMark={requiredMark}
+        onFinish={onFinish}
       >
-        <Form.Item label="اسم الخدمة" required>
+        <Form.Item name="name" label="اسم الخدمة" rules={[{ required: true, message: 'الرجاء ادخال اسم الخدمة' }]} required>
           <Input className="inputAddService" />
         </Form.Item>
-        <Form.Item label="سعر الخدمة" required>
+        <Form.Item name="price" rules={[{ required: true, message: 'الرجاء ادخال سعر الخدمة' }]} label="سعر الخدمة" required>
+          <Input type="number" className="inputAddService" />
+        </Form.Item>
+        <Form.Item name="description" label="الوصف" rules={[{ required: true, message: 'الرجاء ادخال وصف الخدمة' }]} required>
           <Input className="inputAddService" />
         </Form.Item>
-        <Form.Item label="الوصف" required>
-          <Input className="inputAddService" />
+        <Form.Item initialValue name="isFixed" label="السعر ثابت/غيرثابت" required>
+          <Switch defaultChecked />
+          {/* onChange={onChange} */}
+        </Form.Item>
+        <Form.Item initialValue name="isOpen" label="تفعيل / الغاء التفعيل" required>
+          <Switch defaultChecked />
         </Form.Item>
         <Form.Item>
-          <Button type="primary">اضافة خدمة</Button>
+          <Button htmlType="submit" type="primary">اضافة خدمة</Button>
         </Form.Item>
       </Form>
     </div>
