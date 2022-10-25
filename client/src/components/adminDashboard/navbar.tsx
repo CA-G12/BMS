f2e2530bcc93c24252c615752ca92491cc90527e
Mai elhajeen <maielhajeen2001@gmail.com>
2022-10-26 02:28:19 +0300

    adding edit

--- client/src/components/adminDashboard/navbar.tsx
+++ client/src/components/adminDashboard/navbar.tsx
@@ -1,4 +1,4 @@
-import { Layout, Menu } from 'antd';
+import { Layout } from 'antd';
 import logoImg from '../../assets/images/logo.png';
 import userImg from '../../assets/images/user-photo.jpg';
 import './style.css';
@@ -11,9 +11,15 @@ const NavBar: React.FC = () => (
   <Header className="header" style={{ background: '#fff' }}>
     <img src={logoImg as string} alt="logo" className="logo" />
     <div className="top">
-      <p>مرحبًا مي</p>
-      <div className="profile-photo">
-        <img src={userImg as string} alt="user" />
+      <div className="notifications">
+        <i className="ri-notification-3-line" />
+        <div className="notification" />
+      </div>
+      <div className="info">
+        <div className="profile-photo">
+          <img src={userImg as string} alt="user" />
+        </div>
+        <p>مرحبًا مي</p>
       </div>
     </div>
   </Header>
