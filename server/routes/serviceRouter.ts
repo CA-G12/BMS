daf364c7ca4d5268e0d9098cab352639e3282faa
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-25 21:03:55 +0300

    fix eslint in serviceRouter realted #98

--- server/routes/serviceRouter.ts
+++ server/routes/serviceRouter.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { getServices, postServices , updateServices } from '../controllers';
+import { getServices, postServices, updateServices } from '../controllers';
 
 const router = Router();
 
 router.get('/', getServices);
 router.post('/', postServices);
 router.put('/', updateServices);
-
 
 export default router;
