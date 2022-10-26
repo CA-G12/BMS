9827b79270636c8574d36851253e99056d32797a
mohjaps <moh.ja.ps@gmail.com>
2022-10-26 12:51:05 +0300

    init backend all bills

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -4,6 +4,7 @@ export {
 } from './announcements';
 export { getAdvertisements } from './advertisements';
 export { getFlats } from './flats';
-
+export { getAllBills } from './Bills';
+export { sendBillMessage } from './Bills';
 export { getServices, postServices, updateServices } from './services';
 export { signup, login } from './auth';
