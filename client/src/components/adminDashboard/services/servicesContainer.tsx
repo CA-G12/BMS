f75197f7218b1d4c3ed5a12bb50d4e5840852cf2
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-10-26 14:26:04 +0300

    edit the button(is_open) for disable and enable the service related #98

--- client/src/components/adminDashboard/services/servicesContainer.tsx
+++ client/src/components/adminDashboard/services/servicesContainer.tsx
@@ -10,51 +10,72 @@ import { Link } from 'react-router-dom';
 import { InferServicesModel } from '../../../Models/services';
 import { Loading, NoData } from '../../index';
 
-const columns: ColumnsType<InferServicesModel> = [
-  {
-    title: 'اسم الخدمة',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text) => <h3 style={{ color: 'rgb(21 111 193)' }}>{text}</h3>,
-  },
-  {
-    title: 'السعر ثابت/متغير',
-    dataIndex: 'is_fixed',
-    key: 'is_fixed',
-    render: (text) => <p>{text ? 'ثابت' : 'متغير'}</p>,
-  },
-  {
-    title: 'وصف الخدمة',
-    dataIndex: 'description',
-    key: 'description',
-  },
-  {
-    title: 'السعر',
-    dataIndex: 'price',
-    key: 'price',
-  },
-  {
-    title: 'تعديل',
-    dataIndex: 'id',
-    key: 'id',
-    render: (_, record) => (
-      <Link to={`/admin/services/editService/${record.id}`}>
-        <EditOutlined />
-      </Link>
-    ),
-  },
-  {
-    title: 'تفعيل / الغاء التفعيل',
-    dataIndex: 'is_open',
-    key: 'is_open',
-    render: (text) => <Button type="primary">{text ? 'الغاء التفعيل' : 'تفعيل'}</Button>,
-  },
-];
 const { Title } = Typography;
 
 const ServicesContainer: React.FC = () => {
   const [service, setService] = useState<Array<InferServicesModel>>([]);
   const [loading, setLoading] = useState<boolean>(true);
+
+  const columns: ColumnsType<InferServicesModel> = [
+    {
+      title: 'اسم الخدمة',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => <h3 style={{ color: 'rgb(21 111 193)' }}>{text}</h3>,
+    },
+    {
+      title: 'السعر ثابت/متغير',
+      dataIndex: 'is_fixed',
+      key: 'is_fixed',
+      render: (text) => <p>{text ? 'ثابت' : 'متغير'}</p>,
+    },
+    {
+      title: 'وصف الخدمة',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'السعر',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: 'تعديل',
+      dataIndex: 'id',
+      key: 'id',
+      render: (_, record) => (
+        <Link to={`/admin/services/editService/${record.id}`}>
+          <EditOutlined />
+        </Link>
+      ),
+    },
+    {
+      title: 'تفعيل / الغاء التفعيل',
+      dataIndex: 'is_open',
+      key: 'is_open',
+      render: (text, record) => (
+        <Button
+          onClick={() => {
+            // console.log('record: ', record);
+            axios.put(`/api/v1/services/${record.id}`, {
+              id: record.id, name: record.name, isOpen: !record.is_open, isFixed: record.is_fixed, description: record.description, price: record.price,
+            }).then(() => {
+              if (service.length) {
+                setService((prev) => prev.map((ele) => {
+                  if (ele.id === record.id) return { ...record, is_open: !record.is_open };
+                  return ele;
+                }));
+              }
+            }).catch(() => message.error('حدث خطأ , اعد المحاولة'));
+          }}
+          type="primary"
+        >
+          {text ? 'الغاء التفعيل' : 'تفعيل'}
+        </Button>
+      ),
+    },
+  ];
+  // const [isTrue, setIsTrue] = useState<boolean>(true);
   const fetchData = (signal : AbortSignal) => {
     axios.get('/api/v1/services/', { signal })
 
