27f56c3299fc1151842fde6380bb046d3c0d0087
Mai elhajeen <maielhajeen2001@gmail.com>
2022-10-26 12:30:55 +0300

    fix layout

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { LandingPage } from './pages';
-import { AdminDashboard } from './Layouts';
+import { AdminDashboard } from './layouts';
 
 import {
   Services, Bills, Flats,
