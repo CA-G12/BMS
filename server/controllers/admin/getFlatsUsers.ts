d273c85f31ec5e530a976f1641d37a8059f25a43
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-26 07:00:14 +0200

    modified the controller for get flats-user relates #88

--- server/controllers/admin/getFlatsUsers.ts
+++ server/controllers/admin/getFlatsUsers.ts
@@ -7,20 +7,49 @@ import { FlatModel, UserModel } from '../../models';
 export default async (req:Request, res:Response, next:NextFunction) => {
   try {
     const limitNum = 2;
-    const { pageNum } = req.query;
-    if ((+pageNum) <= 0) {
+    const { page } = req.query;
+    const total = await FlatModel.count({ include: [{ model: UserModel }] });
+    if ((+page) <= 0) {
       throw new CustomError(400, 'bad request');
     }
-    const offsetNum = (+(pageNum) - 1) * limitNum;
-    const data = await FlatModel.findAll({
+    const offsetNum = (+(page) - 1) * limitNum;
+    const queryResult : IQueryResult[] = await FlatModel.findAll({
+      raw: true,
+      attributes: ['id', 'flat_number'],
       include: [{
         model: UserModel,
+        attributes: ['first_name', 'last_name', 'phone_number', 'id', 'email'],
       }],
       offset: offsetNum,
       limit: limitNum,
     });
-    res.json({ data });
+
+    const result: FlatUsers[] = queryResult.map((x) => ({
+
+      id: x.id,
+      flat_number: x.flat_number,
+      full_name: `${x['User.first_name']} ${x['User.first_name']}`,
+      phone_number: x['User.phone_number'],
+
+    }));
+    res.json({ result, total });
   } catch (err) {
     next(err);
   }
 };
+
+interface FlatUsers {
+  id: number;
+  full_name: string;
+  flat_number: number;
+  phone_number: string;
+}
+interface IQueryResult {
+  id: number;
+  flat_number: number;
+  // 'User.first_name': string;
+  // 'User.last_name': string;
+  // 'User.phone_number': string;
+  // 'User.id': number;
+  // 'User.email': string;
+}
