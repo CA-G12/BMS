7d9bea35bafacfd92318e6c8984749deca07afaf
bayan2002 <bayanabdelbaryq@gmail.com>
2022-10-30 14:06:35 +0200

    add UserId attribute on FlatsModle relates #107

--- __tests__/updateFlat.ts
+++ __tests__/updateFlat.ts
@@ -13,7 +13,7 @@ describe('Auth API', () => {
             user_id: 2,
         })
         .expect(200);
-      expect(response.body.message).toBe("Updated Successfully");
+      expect(response.body.msg).toBe("Updated Successfully");
     });
     test('Auth -put api/v1/flats/:id', async () => {
       const response = await request(app)
@@ -21,8 +21,8 @@ describe('Auth API', () => {
         .send({
             user_id: 1,
         })
-        .expect(400);
-      expect(response.body.message).toBe("Updated Successfully");
+        .expect(200);
+      expect(response.body.msg).toBe("Updated Successfully");
     });
     test('Auth -put api/v1/flats/:id', async () => {
       const response = await request(app)
@@ -30,9 +30,18 @@ describe('Auth API', () => {
         .send({
             user_id: 1,
         })
-        .expect(400);
+        .expect(404);
       expect(response.body.message).toBe("Not Found");
     });
+    test('Auth -put api/v1/flats/:id', async () => {
+        const response = await request(app)
+          .put('/api/v1/flatsnotFound/1')
+          .send({
+              user_id: 1,
+          })
+          .expect(404);
+        expect(response.body.data.id).toBe(1);
+      });
   });
 
 afterAll(async () => sequelize.close());
