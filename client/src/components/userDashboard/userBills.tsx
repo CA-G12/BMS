38e6d140b112db0697579383f61d8f8e4791ca54
Tareq Hisham <tareqabuaqlain1@gmail.com>
2022-11-05 13:30:15 +0200

    create user bill component static related #133

--- client/src/components/userDashboard/userBills.tsx
+++ client/src/components/userDashboard/userBills.tsx
@@ -1,3 +1,78 @@
-const UserBills = () => <h1>UserBills</h1>;
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable no-nested-ternary */
+import { Typography, message } from 'antd';
+import Table, { ColumnsType } from 'antd/lib/table';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { InferBillUserModel } from '../../Interfaces/billUser';
+import { Loading, NoData } from '../index';
 
+const { Title } = Typography;
+
+const UserBills: React.FC = () => {
+  const [userBill, setUserBill] = useState<Array<InferBillUserModel>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchData = (signal : AbortSignal) => {
+    axios.get('/api/v1/billUser/3', { signal })
+      .then(({ data: { data } }) => {
+        console.log('data: ', data);
+        setUserBill(data as Array<InferBillUserModel>);
+        setLoading(false);
+      }).catch(() => message.error('حدث خطأ , اعد المحاولة'));
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    const controller = new AbortController();
+    const { signal } = controller;
+    fetchData(signal);
+    return () => controller.abort();
+  }, []);
+
+  const columns: ColumnsType<InferBillUserModel> = [
+    {
+      title: 'رقم الشقة',
+      dataIndex: 'flat_number',
+      key: 'flat_number',
+      render: (text) => <h3 style={{ color: 'rgb(21 111 193)' }}>{text}</h3>,
+    },
+    {
+      title: 'السعر',
+      dataIndex: 'userBill[0].FlatId',
+      key: ' userBill[0].total_price',
+      render: (text) => <p>{text}</p>,
+    },
+    {
+      title: 'وصف الخدمة',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'السعر',
+      dataIndex: 'price',
+      key: 'price',
+    },
+  ];
+
+  return (
+    <>
+      <div className="headerOfServices">
+        <Title className="titleAdmin">الفواتير</Title>
+      </div>
+      {
+        (loading) ? <Loading /> : ((userBill.length > 0) ? (
+          <Table
+            pagination={{ defaultPageSize: 6 }}
+            columns={columns}
+            dataSource={userBill}
+          />
+        ) : <NoData />)
+      }
+    </>
+  );
+};
 export default UserBills;
