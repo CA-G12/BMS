264ca601dd76c7b45e6587a4333b658798ee62be
bayan2002 <bayanabdelbaryq@gmail.com>
2022-11-05 19:52:15 +0200

    navigate to home page after loged out

--- client/src/components/adminDashboard/menuList.tsx
+++ client/src/components/adminDashboard/menuList.tsx
@@ -5,7 +5,7 @@ import {
   Button, Layout, Menu, message,
 } from 'antd';
 /* eslint-disable react/prop-types */
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   GroupOutlined,
   ContainerOutlined,
@@ -49,10 +49,12 @@ const listStyle = {
 const MenuList: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
   const { user, setUser } = useContext(authContext) as UserContext;
+  const navigate = useNavigate();
   const Logout = async () => {
     try {
       await axios.get('/api/v1/auth/logout');
       setUser(null);
+      navigate('/');
       return await message.success('تم تسجيل الخروج بنجاح');
     } catch {
       return message.error('حدث خطأ ما');
@@ -65,7 +67,7 @@ const MenuList: React.FC = () => {
     getItem(<Link to="/admin/bills" style={listStyle}>الفواتير</Link>, '4', <DollarCircleOutlined style={listStyle} />),
     getItem(<Link to="/admin/complaints" style={listStyle}>الشكاوي</Link>, '5', <CommentOutlined style={listStyle} />),
     getItem(<Link to="/admin/flats" style={listStyle}>الشقق</Link>, '6', <ContainerOutlined style={listStyle} />),
-    getItem(<Link to="/admin/contacts" style={listStyle}>التواصل</Link>, '6', <MailOutlined style={listStyle} />),
+    getItem(<Link to="/admin/contacts" style={listStyle}>التواصل</Link>, '7', <MailOutlined style={listStyle} />),
     getItem(<Link to="/admin/flats" style={listStyle}>التقيم</Link>, '8', <StarOutlined style={listStyle} />),
     getItem(<Button onClick={Logout} style={{ border: 'none' }}>تسجيل الخروج </Button>, '9', <LogoutOutlined style={listStyle} />),
   ];
