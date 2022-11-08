2b2726093313c2d8c6f6f65be0e5ba5b1ff09339
Mohammed Jaber <moh.ja.ps@hotmail.com>
2022-11-08 12:09:15 +0200

    user ruote authe

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -2,11 +2,11 @@ import { Router } from 'express';
 
 import { addUser, getUsers, profile } from '../controllers';
 import { updateUser } from '../controllers/users';
-import { InferRequestPayload } from '../interfaces/InferUserPayload';
-import { Authorize } from '../middleware';
+// import { InferRequestPayload } from '../interfaces/InferUserPayload';
+// import { Authorize } from '../middleware';
 
 const router = Router();
-router.use((req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'));
+// router.use((req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'));
 
 router.post('/adduser', addUser);
 router.get('/profile', profile);
