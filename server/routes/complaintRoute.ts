ec77d7853341a595770331b9cc17163f5ece4396
Mohammed Jaber <moh.ja.ps@hotmail.com>
2022-11-08 12:10:29 +0200

    complaitns ruote authe

--- server/routes/complaintRoute.ts
+++ server/routes/complaintRoute.ts
@@ -3,17 +3,17 @@ import { Router } from 'express';
 import {
   addComplaints, deleteSingleComplaint, getAllComplaints, getSingleComplaint,
 } from '../controllers';
-import { InferRequestPayload } from '../interfaces/InferUserPayload';
-import { Authorize } from '../middleware';
+// import { InferRequestPayload } from '../interfaces/InferUserPayload';
+// import { Authorize } from '../middleware';
 
 const router = Router();
 
-router.use((req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'));
+// router.use((req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'));
 router.get('/', getAllComplaints);
 router.delete('/:id', deleteSingleComplaint);
 router.get('/:id', getSingleComplaint);
 
-router.use((req, _, next) => Authorize(req as InferRequestPayload, _, next, 'user'));
+// router.use((req, _, next) => Authorize(req as InferRequestPayload, _, next, 'user'));
 router.post('/', addComplaints);
 
 export default router;
