ed570d90398c2e304e199ac3e828b88300a64ce9
Mohammed Jaber <moh.ja.ps@hotmail.com>
2022-11-08 12:12:11 +0200

    announcements ruote authe

--- server/routes/announcementsRouter.ts
+++ server/routes/announcementsRouter.ts
@@ -7,11 +7,11 @@ import { InferRequestPayload } from '../interfaces/InferUserPayload';
 import { Authorize } from '../middleware';
 
 const router = Router();
-
+router.use((req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'));
 router.get('/', getAnnouncement);
-router.post('/', (req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'), postAnnouncement);
-router.delete('/:id', (req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'), deleteAnnouncement);
+router.post('/', postAnnouncement);
+router.delete('/:id', deleteAnnouncement);
 router.get('/:id', getAnnouncementById);
-router.put('/:id', (req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'), putAnnouncement);
+router.put('/:id', putAnnouncement);
 
 export default router;
