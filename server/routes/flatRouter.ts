2f78c2e7d219eabaf81d4cf8c37f8ffdec063ec6
Mohammed Jaber <moh.ja.ps@hotmail.com>
2022-11-08 12:11:05 +0200

    flats ruote authe

--- server/routes/flatRouter.ts
+++ server/routes/flatRouter.ts
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import {
   getFlats, getFlatsUsers, flatById, updateFlat, availableFlats,
 } from '../controllers/index';
-import { InferRequestPayload } from '../interfaces/InferUserPayload';
-import { Authorize } from '../middleware';
+// import { InferRequestPayload } from '../interfaces/InferUserPayload';
+// import { Authorize } from '../middleware';
 
 const router = Router();
-router.use((req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'));
+// router.use((req, _, next) => Authorize(req as InferRequestPayload, _, next, 'admin'));
 
 router.get('/', getFlats);
 router.get('/available', availableFlats);
