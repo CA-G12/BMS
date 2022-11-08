57e3040027b127e103b83412f907c78679fa645f
Mohammed Jaber <moh.ja.ps@hotmail.com>
2022-11-09 01:09:04 +0200

    get advs back end

--- server/controllers/advertisements/getAdvertisemsnts.ts
+++ server/controllers/advertisements/getAdvertisemsnts.ts
@@ -5,7 +5,7 @@ import { AdvertisementModel } from '../../models';
 
 export default async (req:Request, res:Response, next:NextFunction) => {
   try {
-    const data = await AdvertisementModel.findAll({ limit: 3 });
+    const data = await AdvertisementModel.findAll();
 
     res.json({ data });
   } catch (err) {
